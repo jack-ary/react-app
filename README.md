40230b57cd60b201f4bd476a3c30b5ddedc2878f
Jack <jary@calpoly.edu>
2023-10-08 13:35:36 -0700

    finished rest api

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 React app assignment 1 for csc 307
+
+Typically, you use npm run dev while coding your backend, and npm start will be used when you want to run your app in the production environment.
---
+++ packages/express-backend/backend.js
@@ -0,0 +1,122 @@
+// backend.js
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const findUserByName = (name) => { 
+    return users['users_list']
+        .filter( (user) => user['name'] === name); 
+}
+
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+    
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
+});
+
+const addUser = (user) => {
+    users['users_list'].push(user);
+    return user;
+}
+
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
+
+const deleteUser = (req, res) => {
+    const id = req.params.id;
+    const idx = users["users_list"].findIndex((user) => user['id'] === id);
+    if (idx === -1) {
+        res.status(404).send("User not found.");
+    } else {
+        users["users_list"].splice(idx, 1);
+        res.status(204).send(); // Sending a 204 status indicates success without any content.
+    }
+}
+
+app.delete('/users/:id', deleteUser);
+
+app.get('/users', (req, res) => {
+    const name = req.query.name;
+    const job = req.query.job;
+    let filt_users;
+    if (name != undefined){
+        let result = findUserByName(name);
+        filt_users = {users_list: result};
+    }else{
+        filt_users = users;
+    }
+    if (job!=undefined){
+        let result = findUserByJob(filt_users,job);
+        filt_users = {users_list: result};
+    }
+    res.send(filt_users);
+}); 
+
+const users = { 
+    users_list : [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       },
+       {
+        "id": "qwe123",
+        "job": "Zookeeper",
+        "name": "Cindy"
+       },
+    ]
+ }
